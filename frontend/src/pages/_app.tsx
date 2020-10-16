c2289765ea0fd175e12fff130f3fb5dc6c9d8854
Pawel Janus <paweljj@gmail.com>
2020-10-16 23:09:01 +0200

    frontend app scafolding

---
+++ frontend/src/pages/_app.tsx
@@ -0,0 +1,8 @@
+
+function MyApp({ Component, pageProps }: any) {
+  return (
+      <Component {...pageProps} />
+  );
+}
+
+export default MyApp;
---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,5 @@
+const Index = ()=>{
+    return <div>test index</div>
+}
+
+export default Index
